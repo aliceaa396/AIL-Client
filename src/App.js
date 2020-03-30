682d950e22b792a62b617b0e32c62a95cfe8d192
Angel Alicea <ala31996@gmail.com>
2020-03-30 19:25:35 -0400

    refactored Nav component and added a header component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import React, { Component } from 'react'
-import Nav from './Components/Nav/Nav'
-import SideDrawer from './Components/SideDraw/SideDrawer';
-import BackDrop from './Components/BackDrop/BackDrop';
+// import Nav from './Components/Nav/Nav'
+// import SideDrawer from './Components/SideDraw/SideDrawer';
+// import BackDrop from './Components/BackDrop/BackDrop';
+import Header from './Components/Header/Header'
 
 
 
 export class App extends Component {
-  state = {
-    sideDrawerOpen: false
-  };
-
-  drawerToggleClick = () => {
-    this.setState((prevState) => {
-      return {
-        sideDrawerOpen: !prevState.sideDrawerOpen
-      };
-    });
-  };
-
-  backDropToggleClick = () => {
-    this.setState({sideDrawerOpen: false})
-  }
 
   render() {
-    let backDrop;
-
-    if (this.state.sideDrawerOpen) {
-      backDrop = <BackDrop click={this.backDropToggleClick}/>
-    }
+  
 
     return (
       <div style={{height: '100%'}}>
-        <Nav 
-          drawerClickHandler = {this.drawerToggleClick}
-        />
-
-        <SideDrawer 
-          show={this.state.sideDrawerOpen}
-        />
+        <Header />
         
-        {backDrop}
         <main style={{marginTop: '64px'}}>
           <p> PAGE CONTENT </p>
         </main>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Nav from '../Nav/Nav'
+import SideDrawer from '../SideDraw/SideDrawer';
+import BackDrop from '../BackDrop/BackDrop'
+
+export class Header extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClick = () => {
+    this.setState((prevState) => {
+      return {
+        sideDrawerOpen: !prevState.sideDrawerOpen
+      };
+    });
+  };
+
+  backDropToggleClick = () => {
+    this.setState({sideDrawerOpen: false})
+  }
+
+
+  render() {
+    let backDrop;
+
+    if (this.state.sideDrawerOpen) {
+      backDrop = <BackDrop click={this.backDropToggleClick}/>
+    }
+
+    return (
+      <div style={{height: '100%'}}>
+        <Nav 
+          drawerClickHandler = {this.drawerToggleClick}
+        />
+
+        <SideDrawer 
+          show={this.state.sideDrawerOpen}
+        />
+        
+        {backDrop}
+      </div>
+    )
+  }
+}
+
+export default Header
